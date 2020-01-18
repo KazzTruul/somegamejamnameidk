998e322df39a31737c1cb9ad86bac4e8801c01ce
KazzTruul <kazztruul@hotmail.com>
2020-01-18 14:25:41 +0100

    wip

---
+++ ThatGameWereMaking/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    [SerializeField]
+    private int _maxHealth;
+    [SerializeField]
+    protected float _movementSpeed;
+
+    private float _currentHealth;
+
+    protected abstract void Attack();
+
+    protected virtual void Update()
+    {
+
+    }
+
+    private void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected abstract void Die();
+}
